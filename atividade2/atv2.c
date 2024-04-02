03468034a275a2d96012555e3ded7e71e13a0862
ianacaldas <iansonic2003@gmail.com>
2024-04-02 09:55:03 -0300

    add atividade2

---
+++ atividade2/atv2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main (int argc, char* argv[]) {
+
+        if(int i; argv[i] == 0){
+            printf("//false");
+        }
+
+        exit(0);
+    }
